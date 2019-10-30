1f4075ebc208d9a07303b0763f0069f1023a9d6d
Iury Cardoso <iurycardoso6@gmail.com>
2019-10-30 18:28:56 -0300

    Criacao do componente menu

--- starwars/src/app/app.module.ts
+++ starwars/src/app/app.module.ts
@@ -12,11 +12,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
