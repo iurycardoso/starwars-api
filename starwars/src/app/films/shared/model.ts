c554ca5e8b73951778da6764bfb5c23c3c0a5db7
Iury Cardoso <iurycardoso6@gmail.com>
2019-10-30 23:10:42 -0300

    Commit Stage

--- starwars/src/app/films/shared/model.ts
+++
@@ -1,9 +0,0 @@
-export class Films {
-    public title: string;
-    public episode_id: string;
-    public opening_crawl: string;
-    public director: string;
-    public producer: string;
-    public release_data: string;
-
-}
